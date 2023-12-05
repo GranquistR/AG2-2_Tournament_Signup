561b129d54c53bd813edf0d44134c2cbc6470d97
Ryan Granquist <granquistr7820@my.uwstout.edu>
2023-12-05 17:03:14 -0600

    Register displays those registered to tournament

--- register.php
+++ register.php
@@ -8,6 +8,9 @@
   <!-- jQuery include -->
   <script src="/Libraries/jquery-3.7.1.js"></script>
   <link rel="stylesheet" href="Stylesheet/Tournament.css">
+  <!-- Datatable Includes -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+  <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
 
 </head>
 
@@ -21,6 +24,27 @@
       }
       var tournamentId = parseInt(param);
       $('.title').append(tournamentId);
+
+
+      //calls the php as a GET request with params in the url and returns the results as json into the data variable
+      var users = $.get(`PHPRequests/GetRegisteredUserByTournamentID.php?tournamentId=${tournamentId}`, function(users) {
+        //inserts the json response into the datatable
+        for (var i = 0; i < users.length; i++) {
+          var row = "<tr><td>" + users[i].username + "</td><td>" + users[i].email + "</td></tr>";
+          $("#UserDatatable tbody").append(row);
+        }
+        //sets up the datatable and options
+        $('#UserDatatable').DataTable({
+          "paging": true,
+          "info": false,
+          "searching": true,
+          "columnDefs": [{
+            "targets": [1],
+            "orderable": true
+          }]
+        });
+      });
+
     });
   </script>
 
@@ -30,6 +54,22 @@
   <div class="content">
     <a href="/tournaments.php">Back to Tournaments</a>
     <h1 class="title">Registering for tournament id: </h1>
+    Tournament info here
+
+    <!-- registered user table here -->
+    <h1>Current Registered Users</h1>
+    <table id="UserDatatable" class="display">
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Data insterted here -->
+      </tbody>
+    </table>
+
   </div>
 </body>
 

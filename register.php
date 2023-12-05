d89daaa88d31e41a9360febf7b583d18376ad2d3
Mackenzie Kuska <silverwood135831@gmail.com>
2023-12-05 15:55:33 -0600

    partially funcitonal php functions in register

--- register.php
+++ register.php
@@ -1,3 +1,60 @@
+<?php function getTournamentName($tournamentID) //php function that fetches json from sql
+{
+  $config = parse_ini_file('env.config');
+  $host = $config['host'];
+  $db   = $config['db'];
+  $user = $config['user'];
+  $pass = $config['pass'];
+  $charset = 'utf8mb4';
+
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  $opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+  ];
+  $pdo = new PDO($dsn, $user, $pass, $opt);
+
+  //actual sql query here!
+  $sql = "SELECT tournamentName FROM tournament WHERE tournamentID = '".$tournamentID."'";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+
+
+  $results = $stmt->fetchAll();
+  //returns the results as json
+  return json_encode($results);
+}
+?>
+
+<?php function enterTournament($tournament) // php function to join a tournament
+{
+  // access db
+  $config = parse_ini_file('env.config');
+  $host = $config['host'];
+  $db   = $config['db'];
+  $user = $config['user'];
+  $pass = $config['pass'];
+  $charset = 'utf8mb4';
+
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  $opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+  ];
+  $pdo = new PDO($dsn, $user, $pass, $opt);
+
+  // need user
+  $participant = "user1";
+
+  // sql code
+  $sql = "INSERT INTO participatesIn VALUES ($tournament, $participant.participatnID);";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,12 +75,12 @@
         window.location.href = "/tournaments.php";
       }
       var tournamentId = parseInt(param);
-      $('.title').append(tournamentId);
+      $('.title').append(<?php echo getTournamentName($tournamentId['tournamentId']) ?>);
     });
   </script>
 
   <a href="/tournaments.php">Back to Tournaments</a>
-  <h1 class="title">Registering for tournament id: </h1>
+  <h1 class="title">Registering for </h1>
 </body>
 
 </html>
